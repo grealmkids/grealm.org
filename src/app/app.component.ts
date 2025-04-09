41765b68757c5a5d88a5d8d187cfd84b8f773518
ugfire <ugfirecompany@gmail.com>
2025-04-09 11:18:32 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LayoutComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+        { path: '', component: HomeComponent
+           }
+      
+];
